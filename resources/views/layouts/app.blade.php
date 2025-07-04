01d20e9bda4e0faee8aa8aa346c194a5d3ff2d78
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-07-04 21:13:14 +0600

    all pages linked

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>EventEase - @yield('title')</title>
+  <title>EventEaseFork - @yield('title')</title>
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
   @yield('extra-css')
